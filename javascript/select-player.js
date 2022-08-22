ab5bc6c33257affa843a2113d1208a502b64bc0d
Soriful Islam Fahim <islamsoriful248@gmail.com>
2022-08-22 22:49:47 +0600

    complate select player part

---
+++ javascript/select-player.js
@@ -0,0 +1,34 @@
+
+let number = 0;
+const allSelectedButtton = document.getElementsByClassName('selected-button');
+
+for(select of allSelectedButtton){
+    select.addEventListener('click', function(element){
+    number++;
+
+    const playerName = element.target.parentNode.childNodes[3].innerText;
+    // console.log(playerName);
+
+    const selected = element.target.setAttribute('disabled', '');
+
+    if(number > 5){
+       const selected = element.target.removeAttribute('disabled');
+       return alert('You Cannot Add another players')
+    }
+        
+    const selectedPlayers = document.getElementById('total-added-player')
+    const privioustotalSelectedString = selectedPlayers.innerText;
+    const priviousTotalSelected = parseInt(privioustotalSelectedString);
+    selectedPlayers.innerText = number;
+
+    const playersItemBody = document.getElementById('player-items-body');
+
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <th>${number}</th>
+       <td>${playerName}</td>
+    `
+    playersItemBody.appendChild(tr);
+
+}
+)}
--- javascript/select.js
+++ javascript/select.js
@@ -1,26 +1,41 @@
-const arrayPlayer = [];
 
-function displayPlayerName(displayName){
 
-    const tableBody = document.getElementById('player-items');
-    tableBody.innerHTML = '';
-    for(let i = 0; i< displayName.length; i++){
-        const name = arrayPlayer[i];
 
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
-            <th>${i + 1}</th>
-            <td>${name}</td>
-        `
-        tableBody.appendChild(tr);
-    }
-}
+// const arrayPlayer = [];
 
-function selected(element){
-    const playerName = element.parentNode.children[1].innerText;
-    arrayPlayer.push(playerName);
-   
-    document.getElementById('total-added-player').innerText = arrayPlayer.length;
+
+// if(arrayPlayer == 5){
+//     return alert('Your Member Full')
+// }
+
+// function displayPlayerName(displayName){
+
+//     const tableBody = document.getElementById('player-items');
+//     tableBody.innerHTML = '';
+//     for(let i = 0; i < displayName.length; i++){
+
+        
+//         const name = arrayPlayer[i];
+
+//         const tr = document.createElement('tr');
+//         tr.innerHTML = `
+//             <th>${i + 1}</th>
+//             <td>${name}</td>
+//         `
+//         tableBody.appendChild(tr);
+//     }
+// }
+
+// function selected(element){
+//     const playerName = element.parentNode.children[1].innerText;
     
-    displayPlayerName(arrayPlayer);
-}
+//     arrayPlayer.push(playerName);
+
+//     if(arrayPlayer == 5){
+//         return alert('Not')
+//     }
+   
+//     document.getElementById('total-added-player').innerText = arrayPlayer.length;
+
+//     displayPlayerName(arrayPlayer);
+// }
