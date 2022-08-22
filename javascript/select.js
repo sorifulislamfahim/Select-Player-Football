9bcc32c6ee11f9e804c8c950ce0bd1cfec3bcf5f
Soriful Islam Fahim <islamsoriful248@gmail.com>
2022-08-22 18:59:28 +0600

    added player select item

--- javascript/select.js
+++ javascript/select.js
@@ -1,10 +1,26 @@
-
 const arrayPlayer = [];
+
+function displayPlayerName(displayName){
+
+    const tableBody = document.getElementById('player-items');
+    tableBody.innerHTML = '';
+    for(let i = 0; i< displayName.length; i++){
+        const name = arrayPlayer[i];
+
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <th>${i + 1}</th>
+            <td>${name}</td>
+        `
+        tableBody.appendChild(tr);
+    }
+}
+
 function selected(element){
     const playerName = element.parentNode.children[1].innerText;
-    // console.log(playerName);
-
     arrayPlayer.push(playerName);
    
     document.getElementById('total-added-player').innerText = arrayPlayer.length;
+    
+    displayPlayerName(arrayPlayer);
 }
